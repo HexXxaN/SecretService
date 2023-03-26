5319382ea62149681fa8e481912b9e8190e08d51
Szymon Ślubik <szymonslubk@gmail.com>
2023-03-27 00:51:31 +0200

    Created a basic window in int main() using SDL library

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
 #pragma once
+#include "SDL.h"
 
-int main() {
+int main(int argc, char *argv[]) {
 
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("Secret Service", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1200, 768, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+
+	SDL_RenderClear(renderer);
+	while(1)
+		SDL_RenderPresent(renderer);
+
+	return 0;
 }
