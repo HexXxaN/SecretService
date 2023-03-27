94166bf573b194b735bc2e0d8c3f1617c8293f37
Szymon Ślubik <szymonslubik@gmail.com>
2023-03-27 11:07:58 +0200

    Removed .sln file from .gitignore, minor style changes in src files

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "SDL.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
 
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window* window = SDL_CreateWindow("Secret Service", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1200, 768, SDL_WINDOW_SHOWN);
@@ -10,7 +10,8 @@ int main(int argc, char *argv[]) {
 	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
 
 	SDL_RenderClear(renderer);
-	while(1)
+
+	while (1)
 		SDL_RenderPresent(renderer);
 
 	return 0;
