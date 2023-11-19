ce07e853f7ea1708585a9193fe4b55839ff82728
Szymon Ślubik <szymonslubik@gmail.com>
2023-11-19 14:49:34 +0100

    Minor changes to code style

--- src/Point.h
+++ src/Point.h
@@ -15,6 +15,6 @@ struct Point {
 /// <param name="p_point1"> The first Point. </param>
 /// <param name="p_point2"> The second Point. </param>
 /// <returns> True when the points have the same coordinates and false when they do not. </returns>
-inline bool operator== (const Point& p_point1, const Point& p_point2) {
+inline bool operator==(const Point& p_point1, const Point& p_point2) {
 	return p_point1.x == p_point2.x && p_point1.y == p_point2.y;
 }
